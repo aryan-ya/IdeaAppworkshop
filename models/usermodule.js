7cee8622b9b32f821e5041e161edbe9ea10d490f
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-07-07 08:19:26 +0530

    update

---
+++ models/usermodule.js
@@ -0,0 +1,21 @@
+// the will hold the schema for the user if explain thre differents fields of use how  it will be  stored in the mongodb
+
+
+const mongoose = require('mongoose');
+
+const userSchema =  new mongoose.Schema({
+     
+      name : {
+        type : String, 
+        required : true
+      },
+      userID: {
+        type : String,
+        required : true,
+        unique : true
+      },
+      password : {
+        
+      }
+
+});
