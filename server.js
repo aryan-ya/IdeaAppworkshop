82b54fc7b68194a54c36f07a81a0658595305a6b
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-07-07 16:27:51 +0530

    create

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const server = require("./configs/server.configs");
+
 const serverConfigs = require('./configs/server.configs');
 const mongoose = require('mongoose');
 const app =  express();
 const dbConfig = require('./configs/db.configs');
+
+const userModel  =  require('./models/user.model');
+const bcrypt = require('bcrypt');
+
 
 
 // logic to connect to mongodb  Need to have to mongodb to havbing a run machine
@@ -15,9 +19,36 @@ db.on("error",() =>{
     console.log("Error while connecting to DB");
 });
 db.once("open", () =>{
-    console.log("databases is connected");
+    console.log("db is connected");
+
+    init();
 })
 
+
+ async function init() {
+// check if the admin  user is already present
+let admin  =  await userModel.findOne({
+    userID : "admin"
+})
+if (admin) {
+    console.log("Admin user Already present");
+    return;
+}
+ 
+
+
+    // initialize the mongodb
+    admin = await userModel.create( {
+        name : "Aryan yadav",
+        userId : "admin",
+        email : "ay344@gmail.com",
+        userType : "ADMIN",
+        password : bcrypt.hashSync("Welcome1",8)
+    });
+    console.log(admin);
+
+ 
+ }
 app.listen(7777, ()=>{
     console.log(`server started on the port number ${serverConfigs.PORT}`);
-})
+});
